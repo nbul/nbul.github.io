2e6450d9691816234796411e5f88668367f12d41
Natalia Bulgakova <nbul001@gmail.com>
2023-06-21 08:56:51 +0100

    test
    test

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+
+<header>
+ <div class="fixedcontainer">
+	 <div id="links">
+		<div class="linkbox"><a href="index.html">Research</a></div>
+		<div class="linkbox">People</div>
+		<div class="linkbox"><a href="positions.html">Positions available</a></div> 
+		<div class="linkbox"><a href="news.html">News</a></div>
+		<div class="linkbox"><a href="resources.html">Resources</a></div>
+		<div class="linkbox"><a href="publications.html">Publications</a> </div>
+	 </div>
+</div>
+ 
+ <!-- Logo and lab name -->
+ 
+ <div class="header">
+	 <div id="logo"> <p>Bulgakova lab</p>
+	 </div>
+ </div>
+</header>
